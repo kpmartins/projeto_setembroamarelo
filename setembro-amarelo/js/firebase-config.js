10483f1d2cf2dfc11293faac4155682491e69c5b
kpmartins <kaiky.martins2021@gmail.com>
2026-08-27 19:48:22 -0300

    Add files via upload

---
+++ setembro-amarelo/js/firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+// -----------------------------------------------------------------
+// Substitua os valores abaixo pelas credenciais do SEU projeto Firebase.
+// Veja no README.md o passo a passo de como criar um projeto gratuito
+// e pegar essas chaves em: Configurações do projeto > Geral > "Seus apps".
+// -----------------------------------------------------------------
+export const firebaseConfig = {
+  apiKey: "SUA_API_KEY_AQUI",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
